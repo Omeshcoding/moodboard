a10e001b259b1207524726e6cd74d986df2e7b9a
umesh <umesharma.us11@gmail.com>
2024-05-24 11:12:35 +0530

    Created Canvas component, added drawing funtion

---
+++ moodboard/src/components/Canvas.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+
+const Canvas = () => {
+  const canvasRef = useRef(null);
+  const [context, setContext] = useState(null);
+  const [isDrawing, setIsDrawing] = useState(null);
+
+  const styles = {
+    border: '2px solid black',
+    borderRadius: 10,
+  };
+
+  const startDrawing = (e) => {
+    const { offsetX, offsetY } = e.nativeEvent;
+    context.beginPath();
+    context.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+  const draw = (e) => {
+    if (!isDrawing) return;
+    const { offsetX, offsetY } = e.nativeEvent;
+    context.lineTo(offsetX, offsetY);
+    context.stroke();
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 0.9;
+    canvas.height = window.innerHeight * 0.81;
+    const ctx = canvas.getContext('2d');
+    setContext(ctx);
+  }, []);
+  return (
+    <canvas
+      ref={canvasRef}
+      style={styles}
+      onMouseDown={startDrawing}
+      onMouseMove={draw}
+    />
+  );
+};
+
+export default Canvas;
