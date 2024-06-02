f7d13b42b0c2f2f2cf19e3b48c3e0dcdc64683ff
umesh <umesharma.us11@gmail.com>
2024-06-02 22:21:08 +0530

    Added functionality for selection of priests

--- moodboard/src/components/PriestProfile.jsx
+++ moodboard/src/components/PriestProfile.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
 
 const PriestProfile = ({ chatType, name, imgUrl, type, messageType }) => {
+  console.log(chatType);
   return (
     <div
       value={chatType}
-      className="flex flex-wrap  justify-center sm:justify-start items-center my-10 mx-auto h-auto  w-[80%] gap-2 bg-[#FDF0D5] text-[#770C15] rounded-lg px-4 cursor-pointer"
+      className={`flex flex-wrap  justify-center sm:justify-start items-center my-10 mx-auto h-auto  w-[80%] gap-2 ${
+        chatType.type === type ? 'bg-[#fff]' : 'bg-[#FDF0D5]'
+      }  text-[#770C15] rounded-lg px-4 cursor-pointer`}
       onClick={() => messageType({ type: type, name: name, imgUrl: imgUrl })}
     >
       <img
